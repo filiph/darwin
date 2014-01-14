0e67b477aeb93d1a071f3f5a36b47dc33566fbf0
Filip Hracek <filiph@google.com>
2014-01-14 16:08:21 +0100

    Improve README

--- README.md
+++ README.md
@@ -2,3 +2,31 @@ darwin
 ======
 
 A genetic algorithm library for Dart.
+
+Example use:
+
+```dart
+// Create first generation, either by random or by continuing with existing
+// progress.
+var firstGeneration = new Generation<MyPhenotype>()
+  ..members.addAll([/*...*/]);
+
+// Evaluators take each phenotype and assign a fitness value to it according
+// to some fitness function.
+var evaluator = new MyEvaluator();
+
+// Breeders are in charge of creating new generations from previous ones (that
+// have been graded by the evaluator). 
+var breeder = new MyGenerationBreeder()
+  ..crossoverPropability = 0.8;
+
+var algo = new GeneticAlgorithm(firstGeneration, evaluator, breeder);
+
+// Start the algorithm.
+algo.runUntilDone()
+.then((_) {
+  // Print all members of the last generation when done.
+  algo.generations.last.members
+    .forEach((Phenotype ph) => print("${ph.genesAsString}"));
+});
+```
--- lib/src/phenotype.dart
+++ lib/src/phenotype.dart
@@ -1,14 +1,23 @@
 part of darwin;
 
+/**
+ * A phenotype (also called chromosome or genotype) is one particular solution 
+ * to the problem (equation). The solution is encoded in [genes]. 
+ * 
+ * After being evaluated by an [Evaluator], the phenotypes [result] is filled 
+ * with the output of the fitness function. If niching is at play, the result
+ * is then modified into [_resultWithFitnessSharingApplied].
+ * 
+ * Phenotype can have genes of any type [T], although most often, [T] will be
+ * either [bool] (binary genes) or [num] (continuous genes).
+ * 
+ * Subclasses must define [mutateGene], which returns a gene mutated by a given
+ * strength.
+ */
 abstract class Phenotype<T> {
   List<T> genes;
   num result = null;
   num _resultWithFitnessSharingApplied = null;
-  
-//  Phenotype<T> clone() {
-//    Phenotype<T> copy = new Phenotype<T>();
-//    copy.genes = new List<T>.from(genes, growable: false);
-//  }
   
   T mutateGene(T gene, num strength);
   
