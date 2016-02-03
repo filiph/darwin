0421f94418a6d6e5e62aab6301c902e89548805e
Filip Hracek <filip.hracek@gmail.com>
2016-02-03 05:49:14 +0100

    Upgrade unittest to test

--- test/darwin_test.dart
+++ test/darwin_test.dart
@@ -1,4 +1,4 @@
-import "package:unittest/unittest.dart";
+import "package:test/test.dart";
 import "package:darwin/darwin.dart";
 import "dart:math";
 import "dart:async";
@@ -8,14 +8,13 @@ void main() {
   MyEvaluator evaluator;
   GenerationBreeder breeder;
   GeneticAlgorithm algo;
-  
+
   group("Genetic algorithm", () {
-    
     // Set up the variables.
     setUp(() {
       // Create first generation.
       firstGeneration = new Generation<MyPhenotype>();
-      
+
       // Fill it with random phenotypes.
       while (firstGeneration.members.length < 10) {
         var member = new MyPhenotype.Random();
@@ -24,85 +23,76 @@ void main() {
           firstGeneration.members.add(member);
         }
       }
-      
+
       // Evaluators take each phenotype and assign a fitness value to it according
       // to some fitness function.
       evaluator = new MyEvaluator();
-      
+
       // Breeders are in charge of creating new generations from previous ones (that
-      // have been graded by the evaluator). 
+      // have been graded by the evaluator).
       breeder = new GenerationBreeder(() => new MyPhenotype())
         ..crossoverPropability = 0.8;
-      
+
       algo = new GeneticAlgorithm(firstGeneration, evaluator, breeder,
-          printf: (_) {return;}, statusf: (_) {return;});
+          printf: (_) {
+        return;
+      }, statusf: (_) {
+        return;
+      });
     });
-    
-    test("terminates", () {
+
+    test("terminates", () async {
       // Start the algorithm.
-      algo.runUntilDone()
-        .then(expectAsync1((_) {
-          expect(algo.currentGeneration, greaterThan(0));
-        }));
+      await algo.runUntilDone();
+      expect(algo.currentGeneration, greaterThan(0));
     });
-    
-    test("converges to better fitness", () {
+
+    test("converges to better fitness", () async {
       // Start the algorithm.
-      algo.runUntilDone()
-        .then(expectAsync1((_) {
-          // Remember, lower fitness result is better.
-          expect(algo.generations.first.bestFitness,
-              greaterThanOrEqualTo(algo.generations.last.bestFitness));
-        }));
+      await algo.runUntilDone();
+      // Remember, lower fitness result is better.
+      expect(algo.generations.first.bestFitness,
+          greaterThanOrEqualTo(algo.generations.last.bestFitness));
     });
-    
-    test("works without fitness sharing", () {
+
+    test("works without fitness sharing", () async {
       breeder.fitnessSharing = false;
       // Start the algorithm.
-      algo.runUntilDone()
-        .then(expectAsync1((_) {
-          // Remember, lower fitness result is better.
-          expect(algo.generations.first.bestFitness,
-              greaterThanOrEqualTo(algo.generations.last.bestFitness));
-        }));
+      await algo.runUntilDone();
+      // Remember, lower fitness result is better.
+      expect(algo.generations.first.bestFitness,
+          greaterThanOrEqualTo(algo.generations.last.bestFitness));
     });
-    
-    test("works without elitism", () {
+
+    test("works without elitism", () async {
       breeder.elitismCount = 0;
       // Start the algorithm.
-      algo.runUntilDone()
-        .then(expectAsync1((_) {
-          // Remember, lower fitness result is better.
-          expect(algo.generations.first.bestFitness,
-              greaterThanOrEqualTo(algo.generations.last.bestFitness));
-        }));
+      await algo.runUntilDone();
+      // Remember, lower fitness result is better.
+      expect(algo.generations.first.bestFitness,
+          greaterThanOrEqualTo(algo.generations.last.bestFitness));
     });
-    
-    test("onGenerationEvaluatedController works", () {
+
+    test("onGenerationEvaluatedController works", () async {
       // Register the hook;
-      algo.onGenerationEvaluated.listen(expectAsync1((Generation g) {
+      algo.onGenerationEvaluated.listen((Generation g) {
         expect(g.averageFitness, isNotNull);
         expect(g.best, isNotNull);
-      }, max: -1));
-      
+      });
+
       // Start the algorithm.
-      algo.runUntilDone()
-      .then(expectAsync1((_) {
-        // Wait for done.
-      }));
+      algo.runUntilDone();
     });
-    
-    test("Generation.best is assigned to last generation after done", () {
+
+    test("Generation.best is assigned to last generation after done", () async {
       // Start the algorithm.
-      algo.runUntilDone()
-      .then(expectAsync1((_) {
-        expect(algo.generations.last.best, isNotNull);
-      }));
+      await algo.runUntilDone();
+      expect(algo.generations.last.best, isNotNull);
     });
   });
 }
 
-Random random = new Random(); 
+Random random = new Random();
 
 class MyEvaluator extends PhenotypeEvaluator<MyPhenotype> {
   Future<num> evaluate(MyPhenotype phenotype) {
@@ -115,16 +105,16 @@ class MyEvaluator extends PhenotypeEvaluator<MyPhenotype> {
 
 class MyPhenotype extends Phenotype<bool> {
   static int geneCount = 6;
-  
+
   MyPhenotype();
-  
+
   MyPhenotype.Random() {
     genes = new List<bool>(geneCount);
     for (int i = 0; i < geneCount; i++) {
       genes[i] = random.nextBool();
     }
   }
-  
+
   bool mutateGene(bool gene, num strength) {
     return !gene;
   }
